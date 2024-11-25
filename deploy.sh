0d4090a243271ee0bb857b3a35ae22a423b36898
snmkr <sanketnmkr@gmail.com>
2024-11-25 20:08:26 +0530

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Log in to ECR
+
+# Stop and remove the existing container if it exists
+docker stop flask-app || true
+docker rm flask-app || true
+
+AWS_DEFAULT_REGION="ap-south-1"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
+# remove image from local
+docker image rm $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/flask-app:latest
+
+# Pull the latest Docker image
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/flask-app:latest
+
+
+
+# Run the Docker container
+docker run  -d --name flask-app -p 3000:3000 --env-file /home/ec2-user/env.list $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/flask-app:latest
